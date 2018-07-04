0726a52ae85ca0c765e99523115fa117bb0be600
John Oberly <joberly@users.noreply.github.com>
2018-07-04 00:01:08 -0500

    Add bootstrap layout and home and contact views

---
+++ views/view.go
@@ -0,0 +1,35 @@
+package views
+
+import (
+	"html/template"
+	"io"
+)
+
+// NewView creates a new View from the given HTML template files.
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+	)
+
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
+
+// Execute executes the template, writing the results to the io.Writer.
+func (v *View) Execute(wr io.Writer, data interface{}) error {
+	return v.Template.ExecuteTemplate(wr, v.Layout, data)
+}
+
+// View represents a view defined by HTML templates.
+type View struct {
+	Template *template.Template
+	Layout   string
+}
